0ff654dc659e083ab1714eb1902ae9e76736ac23
mleverge <wolfus.ika@gmail.com>
2018-01-25 19:20:29 +0100

    Create chap3.cc

---
+++ Basics/chap3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int age;
+	string name;
+	cout<< "Please enter your name :\n";
+	cin>> name;
+	cout<< "Please indicate your age :\n";
+	cin>> age;
+	cout<< "Hello, " << name << " is " << age << " years old.\n";
+	return 0;
+}
