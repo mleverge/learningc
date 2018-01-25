901589b4d12322111144c5793b2387889019493d
mleverge <wolfus.ika@gmail.com>
2018-01-25 19:50:24 +0100

    Create if.cc

---
+++ Basics/if.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int adulte(0);
+
+	if (adulte == 1 or adulte == 2)
+	{
+    	cout << "You're an adulte !" << endl;
+	}
+	else
+	{
+		cout << "You're a child !\n";
+	}
+	
+	return 0;
+}
